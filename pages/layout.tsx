f714e10849405d5f79d76bb040fcd8f85ea8d8fc
kdgyimah-dev <freelance@kingsleydavid.tech>
2023-09-23 12:52:50 +0000

    :wastebasket: tidying up

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,12 +1,6 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const metadata: Metadata = {
-  title: "Peabux",
-  description: "",
-};
 
 export default function RootLayout({
   children,
